db149979be992c0cef0f6a0f6fb63174ebbca609
wadokh <115899643+wadokh@users.noreply.github.com>
2024-05-18 21:09:23 +0530

    add exercise 3.5 and 3.6

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     { 
         "id": 1,
@@ -23,6 +25,13 @@ let persons = [
         "number": "39-23-6423122"
       }
   ]
+
+  getRandomInt = (min, max) => {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
+  }
+  
 
   app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
@@ -56,6 +65,41 @@ let persons = [
     persons = persons.filter(person => person.id !== id)
 
     response.status(204).end()
+  })
+
+  const checkName = (name) =>{
+    person = persons.find(person => person.name===name)
+    if (person){
+        return true
+    } else {
+        return false
+    }
+  }
+
+  app.post('/api/persons', (request, response) => {
+    const body = request.body
+  
+    if (!body.name || !body.number) {
+      return response.status(400).json({ 
+        error: 'content missing' 
+      })
+    }
+
+    if (checkName(body.name)){
+        return response.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+  
+    const person = {
+      "id": getRandomInt(5,1000000000),
+      "name": body.name,
+      "number": body.number,
+    }
+  
+    persons = persons.concat(person)
+  
+    response.json(person)
   })
   
   const PORT = 3001
