77f9cf81a174b1e35dcb377c857710663bc754ed
wadokh <115899643+wadokh@users.noreply.github.com>
2024-05-18 19:35:10 +0530

    add exercise 3.3

--- index.js
+++ index.js
@@ -38,6 +38,18 @@ let persons = [
                 <br>
                 ${(currentTime)}</p>`)
   })
+
+  app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    
+  
+    if (person) {
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }
+  })
   
   const PORT = 3001
   app.listen(PORT, () => {
