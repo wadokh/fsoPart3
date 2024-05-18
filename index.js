4be659852b293461e7f0ea498049d6bac6bb643c
wadokh <115899643+wadokh@users.noreply.github.com>
2024-05-18 19:47:13 +0530

    add exercise 3.4

--- index.js
+++ index.js
@@ -50,6 +50,13 @@ let persons = [
       response.status(404).end()
     }
   })
+
+  app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
+  })
   
   const PORT = 3001
   app.listen(PORT, () => {
