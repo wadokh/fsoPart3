aa5085e71a0c94c8b7916b118755b6c2dcea3a86
wadokh <115899643+wadokh@users.noreply.github.com>
2024-05-26 20:49:36 +0530

    add 3.15 and 3.16

--- index.js
+++ index.js
@@ -62,17 +62,23 @@ let persons = [
                 ${(currentTime)}</p>`)
   })
 
-  app.get('/api/persons/:id', (request, response) => {
+  app.get('/api/persons/:id', (request, response, next) => {
     Person.findById(request.params.id).then(person => {
-      response.json(person)
+      if (person){
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
     })
+    .catch(error => {error => next(error)})
   })
 
-  app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-
-    response.status(204).end()
+  app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id)
+      .then(result => {
+        response.status(204).end()
+      })
+      .catch(error => next(error))
   })
 
   const checkName = (name) =>{
@@ -108,6 +114,26 @@ let persons = [
       response.json(savedPerson)
     })
   })
+
+  const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+  }
+  
+  // handler of requests with unknown endpoint
+  app.use(unknownEndpoint)
+
+  const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } 
+  
+    next(error)
+  }
+  
+  // this has to be the last loaded middleware, also all the routes should be registered before this!
+  app.use(errorHandler)
   
   const PORT = process.env.PORT
   app.listen(PORT, () => {
